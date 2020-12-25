650f3a7eb9e4c52bdc295aef21f832ed8b8e8fda
Pin Lin <pinlin945@gmail.com>
2020-12-26 03:08:14 +0800

    Ping my computer

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import { config } from 'dotenv';
 import { Telegraf } from 'telegraf';
+import { ping } from './utils/ping';
 
 config();
+
+const IP_ADDRESS = process.env.TARGET_IP_ADDRESS;
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => ctx.reply('Welcome'));
+bot.hears('/ping', async (ctx) => ctx.reply(`Alive: ${await ping(IP_ADDRESS)}`));
 
 bot.launch();
 console.log("Bot launched successfully!")
