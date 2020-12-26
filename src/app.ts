9114a29621aca9d24181a9b505ebe0fb82caf14f
Pin Lin <pinlin945@gmail.com>
2020-12-26 23:06:20 +0800

    Move error handler

--- src/app.ts
+++ src/app.ts
@@ -12,10 +12,6 @@ const SSH_PORT = process.env.TARGET_SSH_PORT;
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.catch((err) => {
-  console.log("Oops, an error occured: ", err);
-});
-
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.hears('/ping', async (ctx) => ctx.reply(`Alive: ${await ping(IP_ADDRESS)}`));
 bot.hears('/hi', async (ctx) => {
@@ -28,5 +24,9 @@ bot.hears('/hi', async (ctx) => {
   return ctx.reply(`STDOUT: ${stdout}`);
 });
 
+bot.catch((err) => {
+  console.log("Oops, an error occured: ", err);
+});
+
 bot.launch();
-console.log("Bot launched successfully!")
+console.log("Bot launched successfully!");
