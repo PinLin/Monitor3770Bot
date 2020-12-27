6af10fe16e4e2624047cadcf91961392f855f392
Pin Lin <pinlin945@gmail.com>
2020-12-27 15:30:53 +0800

    Authorize via allow list

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,28 @@ import { MachineService } from './services/machine-service';
 
 config();
 
+const TOKEN = process.env.BOT_TOKEN;
+const ALLOW_LIST = process.env.BOT_ALLOW_LIST ?? '';
 const IP_ADDRESS = process.env.TARGET_IP_ADDRESS;
 const USERNAME = process.env.TARGET_USERNAME;
 const PASSWORD = process.env.TARGET_PASSWORD;
-const SSH_PORT = process.env.TARGET_SSH_PORT;
+const SSH_PORT = process.env.TARGET_SSH_PORT ?? '22';
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf(TOKEN);
+
+bot.use((ctx, next) => {
+  const id = ctx.from.id;
+  const allowList = ALLOW_LIST.split(',').map((s) => Number(s));
+  if (allowList.includes(id)) {
+    next();
+  } else {
+    const username = ctx.from.username ?? ctx.from.first_name + " " + ctx.from.last_name;
+    console.log(`Denied actions by ${username} (${id})`);
+    ctx.reply(`🔒 此帳號（ \`${id}\` ）不在允許清單內，沒有權限使用。`, {
+      parse_mode: 'Markdown',
+    });
+  }
+});
 
 const machine = new MachineService(IP_ADDRESS, USERNAME, PASSWORD, Number(SSH_PORT));
 const powerStatusController = new PowerStatusController(machine);
