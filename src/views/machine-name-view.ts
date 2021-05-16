63d3fdbaabf7167bf9cb97648073cf6b3ef7f413
Pin Lin <pinlin945@gmail.com>
2021-05-17 01:47:05 +0800

    Simply MachineNameView

--- src/views/machine-name-view.ts
+++ src/views/machine-name-view.ts
@@ -1,20 +1,5 @@
-import { KeyboardButton } from 'telegraf/typings/telegram-types';
-import { BotContext } from '../models/bot-context';
-
 export interface MachineNameViewProps {
   machineName: string;
-  keyboard?: KeyboardButton[][]; // TODO: Remove it.
-}
-
-// TODO: Remove it.
-export function sendMachineNameView(ctx: BotContext, props: MachineNameViewProps) {
-  return ctx.reply(`🖥 *${props.machineName}*`, {
-    parse_mode: 'Markdown',
-    reply_markup: {
-      resize_keyboard: true,
-      keyboard: props.keyboard,
-    },
-  });
 }
 
 export function getMachineNameView(props: MachineNameViewProps) {
