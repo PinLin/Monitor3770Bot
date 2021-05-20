e0fec6920e1497e9ee2966532477e5e50249ffe1
Pin Lin <pinlin945@gmail.com>
2021-05-20 22:31:12 +0800

    Refactor ssh util and daemon

--- src/utils/ssh.ts
+++ src/utils/ssh.ts
@@ -3,21 +3,34 @@ import { EventEmitter } from 'events';
 import { v4 as uuidv4 } from 'uuid';
 import { SshExecutionResult } from '../models/ssh-execution-result';
 
+let sshDaemonIsReady = false;
 let sshDaemon: ChildProcess;
 (function run() {
   sshDaemon = fork(__dirname + '/../daemons/ssh-daemon');
   sshDaemon.on('exit', () => {
+    sshDaemonIsReady = false;
     setTimeout(() => {
       run();
-    }, 1000); // 連線失敗的話每過一秒重試一次
+    }, 1000);
   });
 })()
 
 const resultReceivedEvent = new EventEmitter();
 
+const executionQueue = [] as { uuid: string, command: string }[];
+
 sshDaemon.on('message', (message: string) => {
-  const { uuid, result } = JSON.parse(message) as { uuid: string, result: SshExecutionResult };
-  resultReceivedEvent.emit(uuid, result);
+  const response = JSON.parse(message);
+  if (response.uuid) {
+    resultReceivedEvent.emit(response.uuid, response.result);
+  }
+
+  // 查看是否還有指令要執行
+  if (executionQueue.length > 0) {
+    sshDaemon.send(JSON.stringify(executionQueue.shift()));
+  } else {
+    sshDaemonIsReady = true;
+  }
 });
 
 export function ssh(command: string): Promise<SshExecutionResult> {
@@ -29,11 +42,13 @@ export function ssh(command: string): Promise<SshExecutionResult> {
       res(result);
     });
 
-    // 要求執行指令
-    sshDaemon.send(JSON.stringify({ uuid, command }), (err) => {
-      if (err) {
-        rej(err);
-      }
-    });
+    if (sshDaemonIsReady) {
+      // 送出要執行的指令
+      sshDaemonIsReady = false;
+      sshDaemon.send(JSON.stringify({ uuid, command }));
+    } else {
+      // 把指令放入佇列等待
+      executionQueue.push({ uuid, command });
+    }
   });
 }
