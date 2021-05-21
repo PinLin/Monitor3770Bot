3ec3fa2d8e696bcb71e9bb052bfe05b6dfd52530
Pin Lin <pinlin945@gmail.com>
2021-05-21 08:05:25 +0800

    Register onMessage handler after reconnecting

--- src/utils/ssh.ts
+++ src/utils/ssh.ts
@@ -3,34 +3,33 @@ import { EventEmitter } from 'events';
 import { v4 as uuidv4 } from 'uuid';
 import { SshExecutionResult } from '../models/ssh-execution-result';
 
+const resultReceivedEvent = new EventEmitter();
+
+const executionQueue = [] as { uuid: string, command: string }[];
+
 let sshDaemonIsReady = false;
 let sshDaemon: ChildProcess;
 (function run() {
   sshDaemon = fork(__dirname + '/../daemons/ssh-daemon');
+  sshDaemon.on('message', (message: string) => {
+    const response = JSON.parse(message);
+    if (response.uuid) {
+      resultReceivedEvent.emit(response.uuid, response.result);
+    }
+
+    if (executionQueue.length > 0) {
+      sshDaemon.send(JSON.stringify(executionQueue.shift()));
+    } else {
+      sshDaemonIsReady = true;
+    }
+  });
   sshDaemon.on('exit', () => {
     sshDaemonIsReady = false;
     setTimeout(() => {
       run();
     }, 1000);
   });
-})()
-
-const resultReceivedEvent = new EventEmitter();
-
-const executionQueue = [] as { uuid: string, command: string }[];
-
-sshDaemon.on('message', (message: string) => {
-  const response = JSON.parse(message);
-  if (response.uuid) {
-    resultReceivedEvent.emit(response.uuid, response.result);
-  }
-
-  if (executionQueue.length > 0) {
-    sshDaemon.send(JSON.stringify(executionQueue.shift()));
-  } else {
-    sshDaemonIsReady = true;
-  }
-});
+})();
 
 export function ssh(command: string): Promise<SshExecutionResult> {
   return new Promise((res, rej) => {
